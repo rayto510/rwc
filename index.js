0bb7a4b7bd50f361443cf93e0ccd6711112fa8cf
Raymond To <rto@demandbase.com>
2025-02-05 10:20:00 -0800

    remove commander dependency. use raw nodejs stuff

--- index.js
+++ index.js
@@ -1,60 +1,70 @@
 #!/usr/bin/env node
 
-import { program } from "commander";
 import fs from "fs";
 
-program
-  .version("1.0.0")
-  .description("A utility for counting number of lines, words, characters and bytes in a file.")
-  .option("-c, --count <file>", "Read number of bytes in a file")
-  .option("-l, --lines <file>", "Read number of lines in a file")
-  .option("-w, --words <file>", "Read number of words in a file")
-  .option("-m, --chars <file>", "Read number of characters in a file")
-  .argument("[file]")
-  .action((file, options) => {
-    if (options.count) {
-      try {
-        const stats = fs.statSync(options.count);
-        console.log(`${stats.size} ${options.count}`)
-      } catch (err) {
-        console.error(err);
-      }
-    } else if (options.lines) {
-      try {
-        const data = fs.readFileSync(options.lines, "utf8");
-        const lines = data.split("\n").length;
-        console.log(`${lines} ${options.lines}`);
-      } catch (err) {
-        console.error(err);
-      }
-    } else if (options.words) {
-      try {
-        const data = fs.readFileSync(options.words, "utf8");
-        const wordCount = data.split(/\s+/).filter(w => w.length > 0).length;
-        console.log(`${wordCount} ${options.words}`);
-      } catch (err) {
-        console.error(err);
-      }
-    } else if (options.chars) {
-      try {
-        const data = fs.readFileSync(options.chars, "utf8");
-        const charCount = data.length;
-        console.log(`${charCount} ${options.chars}`);
-      } catch (err) {
-        console.log(err);
-      }
-    } else if (file) {
-      try {
-        const lines = fs.readFileSync(options, "utf8").split("\n").length;
-        const words = fs.readFileSync(options, "utf8").split(/\s+/).filter(w => w.length > 0).length;
-        const chars = fs.readFileSync(options, "utf8").length;
+function countFileContent(data, options, filePath) {
+  try {
+    const lines = data.split("\n").length - 1;
+    const words = data.split(/\s+/).filter((word) => word.length > 0).length;
+    const characters = data.length;
 
-        console.log(`${lines} ${words} ${chars} ${file}`);
-      } catch (err) {
-        console.error(err);
-      }
+    let output = "";
+    if (options.l) output += `  ${lines}`;
+    if (options.w) output += `  ${words}`;
+    if (options.c) output += `  ${characters}`;
+    if (!options.l && !options.w && !options.c)
+      output = `  ${lines}  ${words}  ${characters}`;
 
+    if (filePath) {
+      console.log(`${output} ${filePath}`);
+    } else {
+      console.log(output);
+    }
+  } catch (error) {
+    console.error(`Error reading file: ${error.message}`);
+  }
+}
+
+function convertFileToData(filePath) {
+  return fs.readFileSync(filePath, "utf8");
+}
+
+function parseArgs(args) {
+  const options = { l: false, w: false, c: false };
+  const files = [];
+
+  args.forEach((arg) => {
+    if (arg.startsWith("-")) {
+      if (arg.includes("l")) options.l = true;
+      if (arg.includes("w")) options.w = true;
+      if (arg.includes("c")) options.c = true;
+    } else {
+      files.push(arg);
     }
   });
 
-program.parse(process.argv);
+  return { options, files };
+}
+
+function readStdIn(callback) {
+  let data = "";
+  process.stdin.on("readable", () => {
+    let chunk;
+    while (null !== (chunk = process.stdin.read())) {
+      data += chunk;
+    }
+  });
+
+  process.stdin.on("end", () => callback(data));
+}
+
+const { options, files } = parseArgs(process.argv.slice(2));
+
+if (files.length > 0) {
+  files.forEach((filePath) => {
+    const data = convertFileToData(filePath);
+    countFileContent(data, options, filePath);
+  });
+} else {
+  readStdIn((data) => countFileContent(data, options));
+}
